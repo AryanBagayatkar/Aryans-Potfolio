6a1ddded7dd49f69b122bad0d7bac9131f838920
Aryan Bagayatkar <100182772+AryanBagayatkar@users.noreply.github.com>
2024-12-10 20:04:19 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
       delay: 1
     })
     gsap.from(".hanm2",{
-      y:600,
+      y:40,
       duration : 2,
       delay: 1
     })
@@ -26,14 +26,13 @@ const Hero = () => {
       <div className="col ">
         <h2 className='mt-5 hanm'>Hello World!</h2>
         <h1 className='mt-4 hanm'>Aryan <br /> Bagayatkar</h1>
-                <p className='mt-4 hanm'>Developing a new era web designs and websites with <b>Artifical Intelligence</b>  and <b>3 Dimension Models</b></p>
+        <p className='mt-4 hanm'>Developing a new era web designs and websites with <b>Artifical Intelligence</b> and  <b>3 Dimension Models</b></p>
         <a className='btn btn-dark hanm1' href={pdf1} download="AryanBagayatkar_InternshalaResume.pdf">Resume</a>
       </div>
       <div className="col"></div>
       <div className='shadow-lg mt-5 border-left-0 border border-dark rounded hanm2'>
         <h2 className='p-3'>About Myself</h2>
-        <p className='px-3'><span className='text-primary pl-3'>Hello!</span> <br /> Myself <b>Aryan Bagayatkar </b> <br />A web designer and web developer who resides from Mumbai. I bring a unique prespective to a project . I am also a Video Editor and Graphic Designer, who captures every moment to craft digital Expirence and make it into reality.
-        </p>
+        <p className='px-3'><span className='text-primary pl-3'>Hello!</span> <br /> Myself <b>Aryan Bagayatkar </b> <br /> a web designer and web developer based in Mumbai. I bring a unique perspective to every project. Additionally, I am a video editor and graphic designer, capturing every moment to craft digital experiences and bring them to life.</p>
       </div>
     </div>
   )
