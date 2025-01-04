f101aa051edf32bde4440f69fdf3970e57a6b5ed
Aryan Bagayatkar <100182772+AryanBagayatkar@users.noreply.github.com>
2025-01-04 16:55:37 +0530

    Update Project.jsx

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,68 +5,27 @@ import { Link } from 'react-router-dom';
 import image1 from '../assets/image/img1.png'
 import image2 from '../assets/image/img2.png'
 import image3 from '../assets/image/img3.png'
-import image4 from '../assets/image/img4.png'
+
 
 const Project = () => {
-  useGSAP(()=>{
-    
-    gsap.from(".pnm",{
-      ease: "bounce.out",
-      duration: 1.5,
-      scale: 0.5,
-      stagger: 0.2, 
-    })
-  
-  })
   return (
     <div>
       <section id='projects'>
-        <h2 className='text-center text-secondary'>Projects</h2>
-        <div className="container d-flex ">
-        <div className="row justify-content-center ">
-          <div className=" col-sm-6 col-12 ">
-            <div className="card m-3 pnm" style={{width: '18rem;'}}>
-            <img className="card-img-top" src={image1} alt="Card image cap"/>
-            <div className="card-body">
-              <h5 className="card-title">Gemini Clone</h5>
-              
-              <Link className='btn btn-dark' to="/project1">Take a look</Link>
-              </div>
-            </div>
-          </div>
-          <div className=" col-sm-6 col-12">
-            <div className="card m-3 pnm" style={{width: '18rem;'}}>
-            <img className="card-img-top" src={image2} alt="Card image cap"/>
-            <div className="card-body">
-              <h5 className="card-title">DoubtBot</h5>
-        
-              <Link className='btn btn-dark' to="/project2">Take a look</Link>
-              </div>
-            </div>
-          </div>
-          <div className=" col-sm-6 col-12">
-            <div className="card m-3 pnm" style={{width: '18rem;'}}>
-            <img className="card-img-top" src={image3} alt="Card image cap"/>
-            <div className="card-body">
-              <h5 className="card-title">Lanugage Changer</h5>
-              
-              <Link className='btn btn-dark' to="/project3">Take a look</Link>
-              </div>
-            </div>
-          </div>
-          <div className=" col-sm-6 col-12">
-            <div className="card m-3 pnm" style={{width: '18rem;'}}>
-            <img className="card-img-top" src={image4} alt="Card image cap"/>
-            <div className="card-body">
-              <h5 className="card-title">Collage TimeTable</h5>
-     
-              <Link className='btn btn-dark' to="/project4">Take a look</Link>
-              </div>
-            </div>
-          </div>
-         
-        </div>
-        </div>
+        <h2 className='text-center text-secondary'>Work</h2>
+        <div className="text-center my-5 ">
+     <div className="lg-my-5">
+        <Link to="/project1"><h2 className="my-5 display-2 font-weight-bold">Gemini clone</h2></Link>
+        <Link to="/project1"><img className='w-50 h-50' src={image1} alt="" /></Link>
+      </div>
+      <div className="my-5">
+        <Link to="/project2"><h2 className="my-5 display-2 font-weight-bold">DoubtBot</h2></Link>
+        <Link to="/project2"><img className='img-fluid w-50 h-50' src={image2} alt="" /></Link>
+      </div>
+      <div className="my-5">
+        <Link to="/project3"><h2 className="my-5 display-2 font-weight-bold">Universal Language Converter</h2></Link>
+        <Link to="/project3"><img className='img-fluid w-50 h-50' src={image3} alt="" /></Link>
+      </div>
+    </div>
       </section>
     </div>
   );
