2e855bf6eb092a55936ae7fb3f1b82d677cbfea6
Aryan Bagayatkar <100182772+AryanBagayatkar@users.noreply.github.com>
2025-01-04 16:55:07 +0530

    Update Navbar1.jsx

--- src/components/Navbar1.jsx
+++ src/components/Navbar1.jsx
@@ -1,40 +1,51 @@
-import React, { useEffect } from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import { gsap } from 'gsap';
-import { useGSAP } from '@gsap/react';
-import logo from '../assets/image/logo-d.png'
+import ThemeContext from '../ThemeContext';
 
 function Navbar1() {
-  useGSAP(()=>{
-    
-    gsap.from(".nanm",{
-      y:-600,
-      duration : 2,
-      delay: 1,
-      stagger:0.2
-    })
-  
-  })
+  const { theme, toggleTheme } = useContext(ThemeContext);
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top shadow-lg">
+    <nav
+      className='navbar navbar-expand-lg  sticky-top mx-auto navbar-blur'>
       <div className="container-fluid nanm">
-        <Link className="navbar-brand sm-text-start" to="/"><img src={logo} alt="logo" id='nl'/></Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+        <Link className="navbar-brand sm-text-start" to="/">
+          <h2 className="nav-item ff">Ar<em>y</em>an</h2>
+        </Link>
+        <button
+          className="navbar-toggler collapsed d-flex d-lg-none flex-coloum justify-content-around"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="toggler-icon top-bar"></span>
+          <span className="toggler-icon middle-bar"></span>
+          <span className="toggler-icon bottom-bar"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ">
+          <ul className="navbar-nav">
             <li className="nav-item">
-             <a href="/">Home</a>
+              <a className="nav-link" href="/">Home</a>
             </li>
             <li className="nav-item">
-              <a href="#skill">Skill</a>
+              <Link className="nav-link " to="/contact">Contact</Link>
             </li>
             <li className="nav-item">
-            <a href="#Contact">Contact</a>
+              <Link className="nav-link " to="/project">Work</Link>
             </li>
-            <Link className="nav-link nanm" to="/project">Projects</Link>
           </ul>
+          <div className="theme-toggler mx-auto mx-lg-0 d-block d-lg-inline-block">
+      <label className="switch">
+        <input
+          type="checkbox"
+          onChange={toggleTheme}
+          checked={theme === 'dark'}
+        />
+        <span className="slider round"></span>
+      </label>
+    </div>
         </div>
       </div>
     </nav>
